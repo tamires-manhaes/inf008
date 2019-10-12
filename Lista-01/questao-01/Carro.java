57e4be53b2ff61d33162f13687cd3f592c194070
Tamires <tamiresmanhaes4@gmail.com>
2019-10-11 23:13:29 -0300

    adicionando resolução da questão 01

---
+++ Lista-01/questao-01/Carro.java
@@ -0,0 +1,45 @@
+public class Carro {
+    public String modelo;
+    public float velocidade;
+    public float aceleracao;
+    public int marcha;
+
+    public Carro(){
+        this.modelo = modelo;
+        this.velocidade = velocidade;
+        this.aceleracao = aceleracao;
+        this.marcha = marcha;
+    }
+
+    public void ligar(String modelo){
+        System.out.println("O "+ modelo +"está ligado!");
+    }
+
+    public void desligar(String modelo){
+        System.out.println("O "+modelo+" está desligado");
+    }
+
+    public void acelerar(float velocidade){
+        System.out.println("Acelerando para a velocidade "+ velocidade+"h/km");
+    }
+
+    public void desacelerar(float velocidade){
+        System.out.println("Desacelerando para "+velocidade+"...");
+    }
+
+    public void viraDireita(){
+        System.out.println("Virando a direita...");
+    }
+
+    public void viraEsquerda(){
+        System.out.println("Virando a esqueda...");
+    }
+
+    public void marchaPcima(){
+        System.out.println("Marcha para cima...");
+    }
+
+    public void marchaPbaixo(){
+        System.out.println("Marcha para baixo...");
+    }
+}
---
+++ Lista-01/questao-01/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Carro carro = new Carro();
+	    carro.ligar("honda hrv");
+	    carro.acelerar(60);
+	    carro.viraDireita();
+	    carro.marchaPcima();
+    }
+}
